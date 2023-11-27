5fa35475fda840b8414e794a501c9b74188638ca
Max Cossart <max@maxs-air.lan>
2023-11-27 14:30:44 +1100

    Adds logic to display correct letter and detect whether game has been won

--- src/Game.java
+++ src/Game.java
@@ -6,8 +6,10 @@ public class Game {
     Rules rules = new Rules();
     Scanner scan = new Scanner(System.in);
     LetterValidator checker = new LetterValidator();
+    WordDisplay wordDisplay = new WordDisplay();
 
-    String gameWord = word.Word();
+    // String gameWord = word.Word();
+    String gameWord = "banana";
 
     String letter;
     int chancesLeft = 7;
@@ -15,9 +17,9 @@ public class Game {
     public void begin() {
 
         rules.displayRules();
-        System.out.println(gameWord.length());
+        String gameString = wordDisplay.displayWordStart(gameWord);
 
-        while (chancesLeft >= 0) {
+        while (chancesLeft >= 0 ) {
             System.out.println();
             System.out.println("Please enter a letter");
             letter = scan.nextLine();
@@ -26,6 +28,23 @@ public class Game {
             if(!result) {
                 chancesLeft -= 1;
                 display.printGame(chancesLeft);
+            } 
+
+            gameString = wordDisplay.correctAnswer(letter, gameWord, gameString);
+            System.out.println(gameString);
+
+            if(!gameString.contains("_")) {
+                System.out.println("You win");
+                break;
+            }
+
+            if(chancesLeft == 0) {
+                System.out.println();   
+                System.out.println("Game Over");
+                System.out.println();   
+                System.out.println("The word was: " + gameWord);
+                System.out.println();   
+                break;
             }
         }
     }
--- src/WordDisplay.java
+++ src/WordDisplay.java
@@ -1,6 +1,22 @@
 public class WordDisplay {
 
-    public void displayWord(String word) {
-        System.out.println(word.length());
+    public String displayWordStart(String word) {
+        String display = "";
+        for(int i = 0; i < word.length(); i++) {
+            display += "_ ";
+        }
+        return display;
+    }
+
+    public String correctAnswer(String letter, String word, String gameString) {
+        String[] splitWord = word.split("");
+        String[] displayString = gameString.split(" ");
+
+        for(int i = 0; i < word.length(); i++) {
+            if(letter.matches(splitWord[i])) {
+                displayString[i] = splitWord[i];
+            }
+        }
+        return String.join(" ", displayString);
     }
 }
